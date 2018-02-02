1e7eb4fb61b2eea837f413b9c9829db9d7660043
shahkrima <krima.shah@ves.ac.in>
2018-02-02 10:20:26 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,12 +1,14 @@
 <?php
-$hostname = "localhost";
+$servername = "localhost";
 $username = "root";
 $password = "";
-//connection to the database
-$dbhandle = mysql_connect($hostname, $username, $password) 
-  or die("Unable to connect to MySQL");
-echo "Connected to MySQL<br>";
-//select a database to work with
-$selected = mysql_select_db("startup_buddy",$dbhandle) 
-  or die("Could not select examples");
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+$db=mysqli_select_db($conn, "startup_buddy") or die('MYSQL DATABASE ERROR');
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//else{
+//echo "Connected successfully";}
 ?>
