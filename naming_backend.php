11e15cee76d38436c3bc089f79c5df371b1acce8
shahkrima <krima.shah@ves.ac.in>
2018-02-24 21:53:48 +0530

    Update naming_backend.php

--- naming_backend.php
+++ naming_backend.php
@@ -4,15 +4,18 @@
 table,th,td {
   border : 1px solid black;
   border-collapse: collapse;
+  margin: 0 auto;
 }
 th,td {
   padding: 5px;
+  text-align: center;
 }
 </style>
 </head>
 </html>
 
 <?php
+//error_reporting(0);
 session_start();
 include_once 'connect.php';
 
@@ -20,9 +23,9 @@ if (!isset($_SESSION['userSession'])) {
   header("Location: index.php");
 }
 
-$query = $DBcon->query("SELECT * FROM tbl_users WHERE user_id=".$_SESSION['userSession']);
+$query = $DBcon->query("SELECT * FROM register WHERE user_id=".$_SESSION['userSession']);
 $userRow=$query->fetch_array();
-$DBcon->close();
+
    
    
    if (isset($_GET['names']))
@@ -30,7 +33,7 @@ $DBcon->close();
     $names = $_GET['names'];
     }
    // Escape User Input to help prevent SQL Injection
-   $names = mysql_real_escape_string($names);
+   $names = mysqli_real_escape_string($names);
    
    
 //PHP array containing domain names.
@@ -38,6 +41,7 @@ $domainnames = array(
     '.com',
     '.net',
     '.org',
+    '.gov',
     '.biz',
     '.name',
     '.us',
@@ -88,7 +92,6 @@ $domainnames = array(
     '.org.uk',
     '.me.uk',
     '.me',
-    '.us',
     '.ca',
     '.ac',
     '.academy',
@@ -99,42 +102,108 @@ $domainnames = array(
 
 );
 
+//PHP array containing domain description.
+$domaindesc = array(
+    'Commercial Domain',
+    'Network Domain',
+    'Organization Domain',
+    'Government Domain',
+    'Business Domain',
+    'Name Domain',
+    'United States Domain',
+    'Information Domain',
+    '.in',
+    '.co.in',
+    '.net.in',
+    '.club',
+    '.blog',
+    '.design',
+    '.shop',
+    'Education Domain',
+    '.org.in',
+    '.site',
+    '.online',
+    '.firm.in',
+    '.gen.in',
+    '.ind.in',
+    'European Union Domain',
+    'Mobile Domain',
+    'Mongolia Domain',
+    'Belize Do',
+    'Cocos (Keeling) Islands Domain',
+    'Television Domain',
+    '.eu.com',
+    '.gb.com',
+    '.ae.org',
+    '.kr.com',
+    '.us.com',
+    '.qc.com',
+    '.de.com',
+    '.gb.net',
+    '.no.com',
+    '.hu.com',
+    '.jpn.com',
+    '.uy.com',
+    '.za.com',
+    '.br.com',
+    '.cn.com',
+    '.sa.com',
+    '.se.com',
+    '.se.net',
+    '.uk.com',
+    '.uk.net',
+    '.ru.com',
+    'Samoa Domain',
+    '.co.uk',
+    '.org.uk',
+    '.me.uk',
+    'Montenegro Domain',
+    '.ca',
+    '.ac',
+    '.academy',
+    'United Arab Emirates Domain',
+    '.africa',
+    '.ag',
+    '.art'
+
+);
+
   echo '<table> 
   <tr>      
   <th>Domain Name</th>        
+  <th>Domain Description </th>
   <th>Status</th>      
   </tr>'; 
   
-  foreach($domainnames as $domain) {
+  foreach(array_combine($domainnames, $domaindesc) as $domain => $desc ) {
     echo "<p>";
   $r= $names.$domain;
     $check_name = $DBcon->query("SELECT name FROM naming WHERE name='$r'");
     $count=$check_name->num_rows;
     
-    if ($count==0) {
-       $txt = "Available";
-      //echo "<a href='naming.php'>Next Page</a>";
+    echo '      
+   <tr>             
+   <td>'.$r.'</td> 
+   <td>'.$desc.'</td>  
+    <td>';if ($count==0) {
+       echo "<a href='naming.php'>Available</a>";
  
         }
         else 
         {
-          $txt = "Taken";
+          echo "Taken";
           
         }
-    echo '      
-   <tr>             
-   <td>'.$r.'</td>   
-    <td>'.$txt.' </td>       
+    ' </td>       
       </td> 
     </tr>'; 
   echo "</p>";
-   //<a href="naming.php" target="_blank">A</a>
     }
 echo ' </table>';
    
    
    
    
-   
+   $DBcon->close();
    
 ?>
