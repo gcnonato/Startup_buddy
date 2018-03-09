a9525b9178162a6668d14b453a1d7b77b37c142a
shahkrima <krima.shah@ves.ac.in>
2018-03-09 12:18:36 +0530

    updated

---
+++ recommendation.php
@@ -0,0 +1,26 @@
+<?php
+
+$link = mysqli_connect("localhost", "root", "", "startup_buddy");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+  
+$name = $_POST['name'];
+  $sports = $_POST['sports'];
+  $fashion = $_POST['fashion'];
+  $entertainment = $_POST['entertainment'];
+  $health_and_care = $_POST['health_and_care'];
+  $education = $_POST['eductaion'];
+  $food = $_POST['food'];
+  
+  // Attempt insert query execution
+$sql = "INSERT INTO predict (name,fashion,sports,entartainment,health_and_care,food,education) VALUES ('$name', '$fashion', '$sports','$entertainment','$health_and_care','$food','$education')";
+if(mysqli_query($link, $sql)){
+    echo "Records inserted successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
+  ?>
