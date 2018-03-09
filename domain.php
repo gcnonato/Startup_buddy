6157a054539c8993dc1a66297735529e150785d0
shahkrima <krima.shah@ves.ac.in>
2018-03-09 12:07:21 +0530

    updated

---
+++ domain.php
@@ -0,0 +1,145 @@
+<?php
+include 'header.php';
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Domain</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS 
+    <link href="css/modern-business.css" rel="stylesheet">-->
+    <link href="css/home.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+   
+</head>
+
+<body>
+
+    
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <!-- /.row -->
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="https://in.godaddy.com/">GoDaddy</a>
+                </h2>
+                <hr>
+                <a href="https://in.godaddy.com/">
+                    <img class="img-responsive img-hover" src="images/godaddy.jpg" alt="">
+                </a>
+                <hr>
+                <p>.in domain charges- Rs 149 per year</p>
+                <p>.com domain charges- Rs 299 per year</p>
+                <a class="btn btn-primary" href="https://in.godaddy.com/">Visit Site <i class="fa fa-angle-right"></i></a>
+
+                <hr>
+
+                <!-- Second Blog Post -->
+                <h2>
+                    <a href="https://www.shopify.in/">Shopify</a>
+                </h2>
+                <hr>
+                <a href="https://www.shopify.in/">
+                    <img class="img-responsive img-hover" src="images/shopify.png" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>Basic Plan Rs 1881.95 per month</p>
+                <p>Normal Plan Rs 5126.70 per month</p>
+                 <p>Advance Plan Rs 19403.60 per month</p>
+                  <p>14 days free trial</p>
+                <a class="btn btn-primary" href="https://www.shopify.in/">Visit Site <i class="fa fa-angle-right"></i></a>
+
+                <hr>
+
+                <!-- Third Blog Post -->
+               <h2>
+                    <a href="https://www.bigrock.in/">Bigrock</a>
+                </h2>
+                <hr>
+                <a href="https://www.bigrock.in/">
+                    <img class="img-responsive img-hover" src="images/bigrock.jpg" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>.com domain charges Rs 649 per year</p>
+                 <p>.blog domain charges Rs 599 per year</p>
+                <p>.store Rs 199 per year</p>
+                <a class="btn btn-primary" href="https://www.bigrock.in/">Visit Site <i class="fa fa-angle-right"></i></a>
+
+                <hr>
+
+                 <!-- Fourth Blog Post -->
+               <h2>
+                    <a href="https://www.1and1.com/">1and1.com</a>
+                </h2>
+                <hr>
+                <a href="https://www.1and1.com/">
+                    <img class="img-responsive img-hover" src="images/1and1.jpg" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>.com domain charges  Rs.64.25 for  First year then Rs.972 per year</p>
+                 <p>.org domain charges  Rs.64.25 for  First year then Rs.1297 per year</p>
+                <p>.info domain charges  Rs.64.25 for  First year then Rs.1297 per year</p>
+                <a class="btn btn-primary" href="https://www.1and1.com/">Visit Site <i class="fa fa-angle-right"></i></a>
+
+                <hr>
+                <!-- Fifth Blog Post -->
+               <h2>
+                    <a href="http://hostn.com/">Hostn.com</a>
+                </h2>
+                <hr>
+                <a href="http://hostn.com/">
+                    <img class="img-responsive img-hover" src="images/hostn.png" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>.co domain charges Rs 648 per year</p>
+                 <p>.info domain charges Rs 323 per yearr</p>
+                <p>.com domain charges Rs 649 per year</p>
+                <a class="btn btn-primary" href="http://hostn.com/">Visit Site <i class="fa fa-angle-right"></i></a>
+
+                <hr>
+
+
+            </div>
+
+           
+               
+        </div>
+        <!-- /.row -->
+
+        <?php 
+        include 'footer.php';
+        ?>
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery Version 1.11.0 -->
+    <script src="js/jquery-1.11.0.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
