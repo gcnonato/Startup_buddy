e3265199060fcfb717e06fb69b2e2a0236857aff
shahkrima <krima.shah@ves.ac.in>
2018-03-31 15:08:36 +0530

    updated

--- footer.php
+++ footer.php
@@ -5,12 +5,12 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="css/home.css">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.3.3.7.min.css">
+  <link href="css/Montserrat.css" rel="stylesheet" type="text/css">
+  <link href="css/Lato.css" rel="stylesheet" type="text/css">
+  <script src="js/jquery.3.3.1.min.js"></script>
+  <script src="bootstrap/js/bootstrap.3.3.7.min.js"></script>
+<link rel="stylesheet" href="css/home.css">
   <style>
   .footer {
   
