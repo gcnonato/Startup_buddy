ae54e6a801e4bce888cd878ee831f723f9ef51f3
shahkrima <krima.shah@ves.ac.in>
2018-03-31 15:08:45 +0530

    updated

--- header.php
+++ header.php
@@ -14,14 +14,15 @@ $DBcon->close();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="css/home.css">
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="Keywords" content="Startup, naming, domain purchaser, logo, marketing, internship, success rate">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.3.3.7.min.css">
+  <link href="css/Montserrat.css" rel="stylesheet" type="text/css">
+  <link href="css/Lato.css" rel="stylesheet" type="text/css">
+  <script src="js/jquery.3.3.1.min.js"></script>
+  <script src="bootstrap/js/bootstrap.3.3.7.min.js"></script>
+<link rel="stylesheet" href="css/home.css">
 </head>
 <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
 
@@ -52,7 +53,7 @@ $DBcon->close();
 <li><a href="#">Logo</a></li>
 <li><a href="domain.php">Domain</a></li>
 <li><a href="#">Marketing</a></li>
-<li><a href="#">Hire Interns</a></li>
+<li><a href="postjob.php">Hire Interns</a></li>
 </ul>
 </li>
         
