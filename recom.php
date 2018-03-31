acbe045079d4983a465a51baac463636e1c8ecb6
shahkrima <krima.shah@ves.ac.in>
2018-03-31 15:09:31 +0530

    updated

--- recom.php
+++ recom.php
@@ -13,12 +13,12 @@ include_once 'header.php';
 
 <title>Recommend</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.3.3.7.min.css">
+  <link href="css/Montserrat.css" rel="stylesheet" type="text/css">
+  <link href="css/Lato.css" rel="stylesheet" type="text/css">
+  <script src="js/jquery.3.3.1.min.js"></script>
+  <script src="bootstrap/js/bootstrap.3.3.7.min.js"></script>
+
   <link rel="stylesheet" href="css/home.css">
 
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
