7300217e6ace468c3303f811418e29cedda85ff6
shahkrima <krima.shah@ves.ac.in>
2018-04-07 18:16:55 +0530

    updated

--- sql/project.sql
+++ sql/project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 31, 2018 at 12:01 PM
+-- Generation Time: Apr 07, 2018 at 02:39 PM
 -- Server version: 10.1.24-MariaDB
 -- PHP Version: 7.0.20
 
@@ -187,6 +187,7 @@ CREATE TABLE `post_job` (
   `Email` varchar(60) CHARACTER SET utf8 NOT NULL,
   `Company_Name` varchar(50) NOT NULL,
   `About_Company` varchar(1000) NOT NULL,
+  `Job_Post` varchar(30) NOT NULL,
   `Job_Description` varchar(1000) NOT NULL,
   `No_of_Jobs_Available` int(2) NOT NULL,
   `Who_Can_Apply` varchar(500) NOT NULL,
@@ -200,10 +201,12 @@ CREATE TABLE `post_job` (
 -- Dumping data for table `post_job`
 --
 
-INSERT INTO `post_job` (`Job_Id`, `Email`, `Company_Name`, `About_Company`, `Job_Description`, `No_of_Jobs_Available`, `Who_Can_Apply`, `Start_Date`, `Apply_By`, `Location`, `Type_of_Job`) VALUES
-(1, 'krima.shah@ves.ac.in', 'abc', 'abc', 'abc', 1, 'abc', '2018-04-16', '2018-03-06', 'mumbai', 'Work From Home,Full Time Job,'),
-(2, 'krima.shah@ves.ac.in', 'xyz', 'xyz', 'xyz', 1, 'xyz', '2018-04-20', '2018-03-05', 'nasik', 'Full Time Job,'),
-(5, 'shahkrima2304@gmail.com', 'axw', 'axs', 'dgsk', 2, 'sgs', '2018-03-30', '2018-03-29', 'mumbai', 'Full Time Job,');
+INSERT INTO `post_job` (`Job_Id`, `Email`, `Company_Name`, `About_Company`, `Job_Post`, `Job_Description`, `No_of_Jobs_Available`, `Who_Can_Apply`, `Start_Date`, `Apply_By`, `Location`, `Type_of_Job`) VALUES
+(1, 'krima.shah@ves.ac.in', 'abc', ' abc ', 'manager', ' abc ', 1, 'abc ', '2018-04-16', '2018-03-06', 'mumbai', 'Work From Home,Full Time Job,Internship,'),
+(2, 'krima.shah@gmail.com', 'xyz', ' xyz ', 'GET', ' xyz ', 1, 'xyz ', '2018-04-20', '2018-03-05', 'nasik', 'Work From Home,Full Time Job,'),
+(5, 'bhavna.khatwani@ves.ac.in', 'axw', 'axs', '', 'dgsk', 2, 'sgs', '2018-03-30', '2018-03-29', 'mumbai', 'Full Time Job,'),
+(6, 'dhiren@gmail.com', 'abcd', ' abcd abcd', '', ' abcd  ', 3, 'abcd ', '2018-04-09', '2018-04-03', 'Nasik', 'Work From Home,'),
+(7, 'krima.shah@ves.ac.in', 'krima', 'krima', '', 'krima', 2, 'krima', '2018-04-18', '2018-04-03', 'nasik', 'Work From Home,');
 
 -- --------------------------------------------------------
 
@@ -262,7 +265,10 @@ INSERT INTO `predict` (`predict_id`, `name`, `fashion`, `sports`, `entartainment
 (33, 'df', '', 0, 0, 0, 0, 0),
 (34, 'xg', '', 0, 0, 0, 0, 0),
 (35, 'shilpa', '', 5, 0, 0, 0, 0),
-(36, 'g', '', 0, 0, 0, 0, 0);
+(36, 'g', '', 0, 0, 0, 0, 0),
+(37, 'sonia', '', 1, 0, 0, 0, 0),
+(38, 'k', '', 0, 0, 0, 0, 0),
+(39, 'jayshree', '1', 4, 5, 1, 2, 0);
 
 -- --------------------------------------------------------
 
@@ -305,7 +311,8 @@ ALTER TABLE `naming`
 -- Indexes for table `post_job`
 --
 ALTER TABLE `post_job`
-  ADD PRIMARY KEY (`Job_Id`);
+  ADD PRIMARY KEY (`Job_Id`),
+  ADD UNIQUE KEY `Email` (`Email`,`Job_Post`);
 
 --
 -- Indexes for table `predict`
@@ -334,17 +341,27 @@ ALTER TABLE `naming`
 -- AUTO_INCREMENT for table `post_job`
 --
 ALTER TABLE `post_job`
-  MODIFY `Job_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `Job_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `predict`
 --
 ALTER TABLE `predict`
-  MODIFY `predict_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `predict_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 --
 -- AUTO_INCREMENT for table `register`
 --
 ALTER TABLE `register`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;COMMIT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `post_job`
+--
+ALTER TABLE `post_job`
+  ADD CONSTRAINT `post_job_ibfk_1` FOREIGN KEY (`Email`) REFERENCES `register` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
