3bfddbea17627497abc8ba89129519a0d4c77afe
shahkrima <krima.shah@ves.ac.in>
2018-07-10 13:34:55 +0530

    added

---
+++ m3.php
@@ -0,0 +1,75 @@
+<?php
+include 'header.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Marketing</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/home.css" rel="stylesheet">
+    <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <!-- First Registrar Post -->
+                <h2>
+                    <a href="https://epicenter.epictions.com/epicbeat/explore/">EpicCentre</a>
+                </h2>
+                <hr>
+                <a href="https://epicenter.epictions.com/epicbeat/explore/">
+                    <img class="img-responsive img-hover" src="images/Epicenter.png" alt="">
+                </a>
+                <hr>
+                <p>Epicbeat has a ton of features for researching the latest topic trends, finding engaging content within specific topics and identifying influencers. The free version allows one user and includes unlimited searches as well as the ability to set up one alert, and paid plans start at $39 per month</p>
+                <a class="btn btn-primary" href="https://epicenter.epictions.com/epicbeat/explore/">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+                <!-- Second Registrar Post -->
+                <h2>
+                    <a href="https://www.trendsmap.com/">TrendsMap</a>
+                </h2>
+                <hr>
+                <a href="https://www.trendsmap.com/">
+                    <img class="img-responsive img-hover" src="images/Trendsmap.png" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>With Trendsmap, you can view a visual representation of Twitter trends by geographic location. The free version of Trendsmap provides limited access, but if you find the tool useful, you can upgrade to one of the paid plans, which start at $9 per month.</p>
+                <a class="btn btn-primary" href="https://www.trendsmap.com/">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+                <!-- Third Registrar Post -->
+               <h2>
+                    <a href="http://buzzsumo.com/">BuzzSumo</a>
+                </h2>
+                <hr>
+				<p>BuzzSumo is one of the best tools around for finding the most popular content on a wide range of topics, but it’s also one of the more expensive ones on this list. There is a free version of BuzzSumo with limited functionality, and paid plans start at $99 per month<p>
+                <a a class="btn btn-primary" href="http://buzzsumo.com/">Visit Site <i class="fa fa-angle-right"></i></a>
+                <img class="img-responsive img-hover" src="images/Buzzsumo.jpeg" width"900px" height"300px" alt="">
+                <hr>
+                 <!-- Fourth Registrar Post -->
+               <h2>
+                    <a href="https://trends.google.com/trends/">Google Trends</a>
+                </h2>
+                <hr>
+                <a href="https://trends.google.com/trends/">
+                    <img class="img-responsive img-hover" src="images/Trends.jpeg" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>Unlike most of the tools on this list that base their results on social media metrics, Google Trends reports current and historical organic search trends. Because of this (and because it’s free), it complements our other listed options nicely.</p>
+                 
+                <a class="btn btn-primary" href="https://trends.google.com/trends/">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+                
+        <?php 
+        include 'footer.php';
+        ?>
+    </div>
+    <script src="js/jquery-1.11.0.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
