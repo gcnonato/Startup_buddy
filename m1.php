f89d4a900e19adce70828694ea81e3731cb6ffc8
shahkrima <krima.shah@ves.ac.in>
2018-07-10 13:34:46 +0530

    added

---
+++ m1.php
@@ -0,0 +1,99 @@
+<?php
+include 'header.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Marketing</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/home.css" rel="stylesheet">
+    <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <!-- First Registrar Post -->
+                <h2>
+                    <a href="https://www.google.com/analytics/#?modal_active=none">Google Analytics</a>
+                </h2>
+                <hr>
+                <a href="https://www.google.com/analytics/#?modal_active=none">
+                    <img class="img-responsive img-hover" src="images/Analytics.jpg" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>Google Analytics is a freemium web analytics service offered by Google that tracks and reports website traffic.Google launched the service in November 2005 after acquiring Urchin Google Analytics is now the most widely used web analytics service on the Internet.
+				Google Analytics is offered also in two additional versions: the subscription-based Google Analytics 360, previously Google Analytics Premium, targeted at enterprise users, and Google Analytics for Mobile Apps, an SDK that allows gathering usage data from iOS and Android Apps.</p>
+                
+                <a class="btn btn-primary" href="https://www.google.com/analytics/#?modal_active=none">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+                <!-- Second Registrar Post -->
+                <h2>
+                    <a href="https://clicky.com/">Clicky</a>
+                </h2>
+                <hr>
+                <a href="https://clicky.com/">
+                    <img class="img-responsive img-hover" src="images/Clicky.png" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>Clicky is a complete web analytics tool that lets you see just about everything you could ever want about your website traffic.  It combines historic reporting with real-time website monitoring  in a way that is both easy and elegant. As far as software goes, it's hard to find a better combination of simplicity, power, and value.
+ </p>
+                <a class="btn btn-primary" href="https://www.shopify.in/">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+                <!-- Third Registrar Post -->
+               <h2>
+                    <a href="https://www.woopra.com/">Woopra</a>
+                </h2>
+                <hr>
+                <a href="https://www.woopra.com/">
+                    <img class="img-responsive img-hover" src="images/Woopra.png" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>is a real-time customer analytics service used by sales, service and marketing teams.
+
+The platform is designed to help organizations optimize the customer life-cycle by delivering live, granular behavioral data for individual website visitors and customers.
+ It ties this individual-level data to aggregate analytics reports for a full life-cycle view that bridges departmental gaps.</p>
+                 <a class="btn btn-primary" href="https://www.woopra.com/">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+                 <!-- Fourth Registrar Post -->
+               <h2>
+                    <a href="https://chartbeat.com/">Chartbeat.com</a>
+                </h2>
+                <hr>
+                <a href="https://chartbeat.com/">
+                    <img class="img-responsive img-hover" src="images/Chartbeat.jpeg" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>Chartbeat is a content intelligence company headquartered in New York City. It was started in 2009. The company helps provide actionable insights to the world's leading publishers, media companies and news websites that they can use to make decisions about the content to publish and promote on their sites.In August 2010, the company was spun off from Betaworks as a separate company.
+				Chartbeat has been touted and critiqued as an alternative to Google Analytics for real-time data</p>
+                 
+                <a class="btn btn-primary" href="https://chartbeat.com/">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+                <!-- Fifth Registrar Post -->
+               <h2>
+                    <a href="https://www.usertesting.com/">User Testing.com</a>
+                </h2>
+                <hr>
+                <a href="https://www.usertesting.com/">
+                    <img class="img-responsive img-hover" src="images/User.jpeg" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>Usability testing has never been easier. Get videos of real people speaking their thoughts as they use websites, mobile apps, prototypes and more!</p>
+                 
+				 <a class="btn btn-primary" href="https://www.usertesting.com/">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+            </div>    
+        </div>
+        <?php 
+        include 'footer.php';
+        ?>
+    </div>
+    <script src="js/jquery-1.11.0.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
