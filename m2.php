90bb20db4f92b52f130bc93e0bfa62e1fed2e90c
shahkrima <krima.shah@ves.ac.in>
2018-07-10 13:34:50 +0530

    added

---
+++ m2.php
@@ -0,0 +1,105 @@
+<?php
+include 'header.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Marketing</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/home.css" rel="stylesheet">
+    <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <!-- First Registrar Post -->
+                <h2>
+                    <a href="https://www.google.co.in/adwords/?channel=ha-ef-bk&sourceid=awo&subid=in-en-ha-g-aw-c-bkhv_1!o2~-1079355572-196561704135&gclid=EAIaIQobChMI0v_a4LnO2gIVjw4rCh1-xgH8EAAYASAAEgLQsfD_BwE&dclid=CLfF_-S7ztoCFQSfaAodWk4Mvg">DigitalPoint</a>
+                </h2>
+                <hr>
+                <a href="https://www.google.co.in/adwords/?channel=ha-ef-bk&sourceid=awo&subid=in-en-ha-g-aw-c-bkhv_1!o2~-1079355572-196561704135&gclid=EAIaIQobChMI0v_a4LnO2gIVjw4rCh1-xgH8EAAYASAAEgLQsfD_BwE&dclid=CLfF_-S7ztoCFQSfaAodWk4Mvg">
+                    <img class="img-responsive img-hover" src="images/Digital.jpeg" alt="">
+                </a>
+                <hr>
+                <p>Google AdWords is an online advertising service developed by Google, where advertisers pay to display brief advertising copy, product listings, and video content within the Google ad network to web users. Google AdWords' system is based partly on cookies and partly on keywords determined by advertisers. Google uses these characteristics to place advertising copy on pages where they think it might be relevant. Advertisers pay when users divert their browsing to click on the advertising copy. Partner websites receive a portion of the generated income</p>
+                
+                <a class="btn btn-primary" href="https://www.google.co.in/adwords/?channel=ha-ef-bk&sourceid=awo&subid=in-en-ha-g-aw-c-bkhv_1!o2~-1079355572-196561704135&gclid=EAIaIQobChMI0v_a4LnO2gIVjw4rCh1-xgH8EAAYASAAEgLQsfD_BwE&dclid=CLfF_-S7ztoCFQSfaAodWk4Mvg">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+                <!-- Second Registrar Post -->
+                <h2>
+                    <a href="https://www.grin.co/#features">Grin</a>
+                </h2>
+                <hr>
+                <a href="https://www.grin.co/#features">
+                    <img class="img-responsive img-hover" src="images/Grin.png" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>Influencers are social media leaders with a loyal following. The most notable influencers are millennial queens like Selena Gomez, Gigi Hadid, and Kylie Jenner. There are much, much more influencers from musicians to athletes to doctors to podcasters and everything in between.
+
+Influencers connect with their followers by producing organic content which consumers find much more authentic and trustworthy than the typical branded ad.</p>
+                <a class="btn btn-primary" href="https://www.grin.co/#features">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+                <!-- Third Registrar Post -->
+               <h2>
+                    <a href="https://www.facebook.com/business/learn/facebook-ads-reporting-ads-manager">Facebook Ads</a>
+                </h2>
+                <hr>
+                <a href="https://www.facebook.com/business/learn/facebook-ads-reporting-ads-manager">
+                    <img class="img-responsive img-hover" src="images/Facebook.jpg" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>There's a reason that 100% of the top 100 brands worldwide have run ads on YouTube in the past year. It works.
+
+Similar to influencer marketing, YouTube ads allow you to target very specific demographics who are loyal followers of their favorite YouTube channels.
+
+You can also catch a ton of customer leads by placing ads on video-driven, rather than channel driven YouTube accounts.</p>
+                <a class="btn btn-primary" href="https://www.facebook.com/business/learn/facebook-ads-reporting-ads-manager">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+                 <!-- Fourth Registrar Post -->
+               <h2>
+                    <a href="https://medium.com/comms-planning/11-branded-snapchat-filters-that-worked-94a808afa682">Snapchat filter.com</a>
+                </h2>
+                <hr>
+                <a href="https://medium.com/comms-planning/11-branded-snapchat-filters-that-worked-94a808afa682">
+                    <img class="img-responsive img-hover" src="images/Snapchat.jpeg" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>Sponsored Snapchat filters are a great way to get consumers to interact with your brand and promote your ad content to their own following.
+
+Big brands shell out as much as $750,000 a day to feature their customized filter.
+
+Why is this such a powerful ad platform? Enjoyability.
+
+Snapchatters use filters because it's fun to use the face-morphing designs. With a fun and creative sponsored filter, you can reach millions of viewers by appealing to their sense of play</p>
+                <a class="btn btn-primary" href="https://medium.com/comms-planning/11-branded-snapchat-filters-that-worked-94a808afa682">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+                <!-- Fifth Registrar Post -->
+               <h2>
+                    <a href="https://buffer.com/">Hostn.com</a>
+                </h2>
+                <hr>
+                <a href="https://buffer.com/">
+                    <img class="img-responsive img-hover" src="images/Hostn1.png" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>Content schedulers like Buffer give you a lot of power over your social ad campaigns.
+
+Pre-scheduling tweets, statuses, photos, and other content allows you to take control of your time while still reaping the benefits of an optimized posting time.</p>
+                 <<a class="btn btn-primary" href="https://buffer.com/">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+            </div>    
+        </div>
+        <?php 
+        include 'footer.php';
+        ?>
+    </div>
+    <script src="js/jquery-1.11.0.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
