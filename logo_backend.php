4325dd5e6b21ab5015f9ac83714d5f7d9fda03e8
shahkrima <krima.shah@ves.ac.in>
2018-07-10 13:35:42 +0530

    added

---
+++ logo_backend.php
@@ -0,0 +1,227 @@
+<?php 
+ if (isset($_GET['names']))
+    {
+    $names = $_GET['names'];
+    }
+    
+  
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
+.w3-bar-block .w3-bar-item {padding:20px}
+
+.btn {
+    background-color: DodgerBlue;
+    border: none;
+    color: white;
+    padding: 12px 30px;
+    font-size: 16px;
+    cursor: pointer;
+    font-size: 20px;
+}
+
+/* Darker background on mouse-over */
+.btn:hover {
+    background-color: RoyalBlue;
+}
+</style>
+</head>
+<body>
+
+  <h4 style="text-align: center;">Logo</h4>
+  <?php 
+if ($names=='purse')
+{
+  ?>
+ <!-- First Photo Grid-->
+  <div class="w3-row-padding w3-padding-16 w3-center" id="logo">
+    <div class="w3-quarter">
+      <a download="images/logo.png" href="images/logo.png" >
+    <img alt="Logo" src="images/logo.png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+</div>
+    <div class="w3-quarter">
+      <a download="images/logo (1).png" href="images/logo (1).png" >
+    <img alt="Logo" src="images/logo (1).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+</div>
+    <div class="w3-quarter">
+      <a download="images/logo (2).png" href="images/logo (2).png" >
+    <img alt="Logo" src="images/logo (2).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+</div>
+    <div class="w3-quarter">
+      <a download="images/logo (3).png" href="images/logo (3).png" >
+    <img alt="Logo" src="images/logo (3).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+    </div>
+  </div>
+  
+  <!-- Second Photo Grid-->
+  <div class="w3-row-padding w3-padding-16 w3-center">
+    <div class="w3-quarter">
+      <a download="images/logo (4).png" href="images/logo (4).png" >
+    <img alt="Logo" src="images/logo (4).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+    </div>
+    <div class="w3-quarter">
+     <a download="images/logo (5).png" href="images/logo (5).png" >
+    <img alt="Logo" src="images/logo (5).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+      </div>
+    <div class="w3-quarter">
+      <a download="images/logo (6).png" href="images/logo (6).png" >
+    <img alt="Logo" src="images/logo (6).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+</div>
+    <div class="w3-quarter">
+     <a download="images/logo (7).png" href="images/logo (7).png" >
+    <img alt="Logo" src="images/logo (7).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+    </div>
+  </div>
+<!-- Third Photo Grid-->
+  <div class="w3-row-padding w3-padding-16 w3-center">
+    <div class="w3-quarter">
+      <a download="images/logo (8).png" href="images/logo (8).png" >
+    <img alt="Logo" src="images/logo (8).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+    </div>
+    <div class="w3-quarter">
+      <a download="images/logo (9).png" href="images/logo (9).png" >
+    <img alt="Logo" src="images/logo (9).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+    </div>
+    <div class="w3-quarter">
+      <a download="images/logo (10).png" href="images/logo (10).png" >
+    <img alt="Logo" src="images/logo (10).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+    </div>
+    <div class="w3-quarter">
+      <a download="images/logo (11).png" href="images/logo (11).png" >
+    <img alt="Logo" src="images/logo (11).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+      
+    </div>
+  </div>
+
+<?php
+}
+elseif ($names=='shoe') {
+?>
+<!-- First Photo Grid-->
+  <div class="w3-row-padding w3-padding-16 w3-center" id="logo">
+    <div class="w3-quarter">
+      <a download="images/logo (12).png" href="images/logo (12).png" >
+    <img alt="Logo" src="images/logo (12).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+</div>
+    <div class="w3-quarter">
+      <a download="images/logo (13).png" href="images/logo (13).png" >
+    <img alt="Logo" src="images/logo (13).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+</div>
+    <div class="w3-quarter">
+      <a download="images/logo (14).png" href="images/logo (14).png" >
+    <img alt="Logo" src="images/logo (14).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+</div>
+    <div class="w3-quarter">
+      <a download="images/logo (15).png" href="images/logo (15).png" >
+    <img alt="Logo" src="images/logo (15).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+    </div>
+  </div>
+  
+  <!-- Second Photo Grid-->
+  <div class="w3-row-padding w3-padding-16 w3-center">
+    <div class="w3-quarter">
+      <a download="images/logo (16).png" href="images/logo (16).png" >
+    <img alt="Logo" src="images/logo (16).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+    </div>
+    <div class="w3-quarter">
+     <a download="images/logo (17).png" href="images/logo (17).png" >
+    <img alt="Logo" src="images/logo (17).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+      </div>
+    <div class="w3-quarter">
+      <a download="images/logo (18).png" href="images/logo (18).png" >
+    <img alt="Logo" src="images/logo (18).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+</div>
+    <div class="w3-quarter">
+     <a download="images/logo (19).png" href="images/logo (19).png" >
+    <img alt="Logo" src="images/logo (19).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+    </div>
+  </div>
+<!-- Third Photo Grid-->
+  <div class="w3-row-padding w3-padding-16 w3-center">
+    <div class="w3-quarter">
+      <a download="images/logo (20).png" href="images/logo (20).png" >
+    <img alt="Logo" src="images/logo (20).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+    </div>
+    <div class="w3-quarter">
+      <a download="images/logo (21).png" href="images/logo (21).png" >
+    <img alt="Logo" src="images/logo (21).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+    </div>
+    <div class="w3-quarter">
+      <a download="images/logo (22).png" href="images/logo (22).png" >
+    <img alt="Logo" src="images/logo (22).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+    </div>
+    <div class="w3-quarter">
+      <a download="images/logo (23).png" href="images/logo (23).png" >
+    <img alt="Logo" src="images/logo (23).png" style="width:100%; height: 250px">
+    <button class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+      
+    </div>
+  </div>
+  
+<?php
+}
+  ?>
+
+
+
+</body>
+</html>
+
+
