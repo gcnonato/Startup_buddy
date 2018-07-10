0ff02bc3f9b6e34a1c070004cac3876c7420e274
shahkrima <krima.shah@ves.ac.in>
2018-07-10 13:35:00 +0530

    added

---
+++ m4.php
@@ -0,0 +1,81 @@
+<?php
+include 'header.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Marketing</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/home.css" rel="stylesheet">
+    <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <!-- First Registrar Post -->
+                <h2>
+                    <a href="https://developers.google.com/speed/pagespeed/insights/">Google Pagespeed</a>
+                </h2>
+                <hr>
+                <a href="https://developers.google.com/speed/pagespeed/insights/">
+                    <img class="img-responsive img-hover" src="images/Pagespeed.png" alt="">
+                </a>
+                <hr>
+                <p>Enter a URL, and this tool will test the loading time and performance for desktop and for mobile, plus identify opportunities to improve (and pat you on the back for what you’re doing well). The mobile results also come with a user experience score, grading areas like tap targets and font sizes.</p>
+                <p>.com domain charges- Rs 299 per year</p>
+                <a class="btn btn-primary" href="https://developers.google.com/speed/pagespeed/insights/">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+                <!-- Second Registrar Post -->
+                <h2>
+                    <a href="https://moz.com/local/search">Moz</a>
+                </h2>
+                <hr>
+                <a href="https://moz.com/local/search">
+                    <img class="img-responsive img-hover" src="images/Moz.png" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>Moz crunches data from more than 15 different sources—including Google, Foursquare, and Facebook—to score your brick-and-mortar business on how it looks online. Results come complete with actionable fixes for inconsistent or incomplete listings.</p>
+                
+                <a class="btn btn-primary" href="https://moz.com/local/search">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+                <!-- Third Registrar Post -->
+               <h2>
+                    <a href="https://keywordtool.io/">Keyword Tool</a>
+                </h2>
+                <hr>
+                <a href="https://keywordtool.io/">
+                    <img class="img-responsive img-hover" src="images/Keyword.jpeg" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>700+ keyword ideas based on a single keyword
+
+Enter a keyword, and the Keyword Tool provides a huge handful of long-tail keyword opportunities, organized alphabetically</p>
+                <a class="btn btn-primary" href="https://keywordtool.io/">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+                 <!-- Fourth Registrar Post -->
+               <h2>
+                    <a href="https://www.googlewebmastertool.com/">Google Web Master</a>
+                </h2>
+                <hr>
+                <a href="https://www.googlewebmastertool.com">
+                    <img class="img-responsive img-hover" src="images/Webmaster.jpeg" width"900px" height"300px" alt="">
+                </a>
+                <hr>
+                <p>These webmaster tools help give you a taste of what the two top search engines think of your site. It’s helpful to see any bugs, alerts, and indexing issues.</p>
+                 
+                <a class="btn btn-primary" href="https://www.googlewebmastertool.com">Visit Site <i class="fa fa-angle-right"></i></a>
+                <hr>
+                    
+        </div>
+        
+    </div>
+    <script src="js/jquery-1.11.0.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
